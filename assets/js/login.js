b17179e47d617c2c48a273dce4874609fd7cd62c
“Gbolahan <gbolahansalvador37@gmail.com>
2025-10-05 14:37:32 +0100

    i did the hide and unhide password button with javascript

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const description = document.querySelector(".login-descript");
   const submitButton = document.querySelector(".submit-btn");
   const customerButton = document.querySelector(".active");
+  const eye = document.querySelector(".unhide-eye")
+  const passwordInput = document.querySelector(".password-input")
+  const slashEye = document.querySelector(".hide-eye")
 
   function change() {
     description.innerText = "Complete errands and earn money";
@@ -20,4 +23,20 @@ document.addEventListener("DOMContentLoaded", () => {
     customerButton.style.backgroundColor = "#FFFFFF"
   }
   customerButton.addEventListener("click", change2);
+
+  function hide (){
+      passwordInput.type = "Text"
+      slashEye.style.display = "block"
+      eye.style.display = "none"
+  }
+  eye.addEventListener("click", hide)
+
+  function unhide (){
+      passwordInput.type = "password"
+      slashEye.style.display = "none"
+      eye.style.display = "block"
+  }
+  slashEye.addEventListener("click", unhide)
 });
+
+
