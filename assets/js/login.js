3a35f7dfdd8c07972d2d005c4c3973550ebd5dfa
“Gbolahan <gbolahansalvador37@gmail.com>
2025-10-20 23:12:20 +0100

    i added detail page for services

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,21 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const eye = document.querySelector(".unhide-eye")
-  const passwordInput = document.querySelector(".password-input")
-  const slashEye = document.querySelector(".hide-eye")
+const eye = document.querySelector(".unhide-eye")
+const passwordInput = document.querySelector(".password-input")
+const slashEye = document.querySelector(".hide-eye")
 
-  function hide (){
-      passwordInput.type = "Text"
-      slashEye.style.display = "block"
-      eye.style.display = "none"
-  }
+function hide (){
+  passwordInput.type = "Text"
+  slashEye.style.display = "block"
+  eye.style.display = "none"
+}
   eye.addEventListener("click", hide)
 
-  function unhide (){
-      passwordInput.type = "password"
-      slashEye.style.display = "none"
-      eye.style.display = "block"
-  }
+
+function unhide (){
+  passwordInput.type = "password"
+  slashEye.style.display = "none"
+  eye.style.display = "block"
+}
+
   slashEye.addEventListener("click", unhide)
+  
 });
 
 
